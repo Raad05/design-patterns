c871675a602c60403ec4617c758ac73c34bad264
Raad05 <ashabulraad@gmail.com>
2024-05-18 00:38:57 +0600

    tax calculator interface example

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,13 @@ public class Main {
         User user = new User("Raad");
 
         user.greet();
+
+        TaxCalculator calculator = getCalculator();
+
+        System.out.println(calculator.calculateTax());
+    }
+
+    public static TaxCalculator getCalculator() {
+        return new TaxCalculator2024();
     }
 }
